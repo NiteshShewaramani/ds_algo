ad9dfe8c6bbb1c3cbb8cc6315743fb76eb47577c
NiteshShewaramani <40917774+NiteshShewaramani@users.noreply.github.com>
2023-12-05 08:07:17 +0530

    adding quick union weighted and path compression
    adding quick union weighted and path compression  optimal methods to solve union find problems

---
+++ ds_algo/WeightedQuickUnionAndPathCompression.java
@@ -0,0 +1,67 @@
+package com.example.CoinGame;
+
+public class WeightedQuickUnionAndPathCompression {
+
+    int arr[];
+    int size[];
+
+    public WeightedQuickUnionAndPathCompression(int n)
+    {
+        arr = new int[n];
+        size = new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=i;
+            size[i]=1;
+        }
+
+    }
+    public int root(int p)
+    {
+        //searching for the top root element here
+        while(p != arr[p])
+        {
+            ///this will point to grand parent of the current parent
+            arr[p] = arr[arr[p]];
+            ///we are making every node point to its grandparent
+            //we are chaning array data
+            //this will keep the tree almost flat
+            p=arr[p];
+
+        }
+        return p;
+
+    }
+    public boolean isConnected(int p,int q)
+    {
+        return root(p) == root(q);
+    }
+
+    /**
+     * union(4,5)
+     * the root of element 4 will now point to root of 5 , it will create a tree structure
+     * this way all the elements connected to 4 will now be connected to tree of element 5th
+     * @param p
+     * @param q
+     */
+    public void union(int p,int q){
+        //change the root
+        int i = root(p);
+        int j = root(q);
+        if(i == j)
+            return;
+        // due to size array we will always
+        //point the lower tree under root of bigger tree
+        if(size[i] > size[j])
+        {
+            arr[j] = i;
+            size[i] =  size[i] + size[j];
+        }
+        else
+        {
+            arr[i]=j;
+            size[j] = size[j] + size[i];
+        }
+        arr[i]=j;
+    }
+}
